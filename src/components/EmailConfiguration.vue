be1de87f05cd76f034a370515f656a68ae38cbd8
David-Pena <dev.davidpe@gmail.com>
2024-10-27 15:55:06 -0700

    chore(settings): remove ring styles in ThemeToggle switch

--- src/components/EmailConfiguration.vue
+++ src/components/EmailConfiguration.vue
@@ -37,14 +37,15 @@ const emailProviders: EmailProvider[] = [
           Configuración de correo electrónico
         </h2>
         <p class="text-sm text-gray-500 dark:text-gray-400">
-          Configure las configuraciones de correo electrónico para las notificaciones de eventos
+          Configure las configuraciones de correo electrónico para las notificaciones de
+          eventos
         </p>
       </div>
       <div class="flex items-center">
         <label class="relative inline-flex items-center cursor-pointer">
           <input type="checkbox" v-model="emailEnabled" class="sr-only peer" />
           <div
-            class="w-11 h-6 bg-gray-200 dark:bg-gray-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 dark:after:border-gray-600 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary-600 dark:peer-checked:bg-primary-500"
+            class="w-11 h-6 bg-gray-200 dark:bg-gray-700 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 dark:after:border-gray-600 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary-dark dark:peer-checked:bg-primary-light focus:outline-none"
           ></div>
         </label>
       </div>
