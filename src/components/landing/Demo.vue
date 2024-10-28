bdd0825f91360ade33fc766e82ae9375ba30b4c8
David-Pena <dev.davidpe@gmail.com>
2024-10-27 23:39:09 -0700

    chore: translate to spanish demo video fallback

--- src/components/landing/Demo.vue
+++ src/components/landing/Demo.vue
@@ -66,7 +66,7 @@ const demoVideoUrl = ref();
                     d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
                   />
                 </svg>
-                <p class="text-lg">Demo video coming soon</p>
+                <p class="text-lg">Video próximamente</p>
               </div>
             </div>
           </div>
